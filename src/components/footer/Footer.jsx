cf0eb161e41ca88411b4ece19d610cd691b25379
longslim <longslim4real@gmail.com>
2025-05-15 04:48:11 -0700

    final commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -112,7 +112,7 @@ function Footer() {
         <h2>FEEDBACK</h2>
         <form onSubmit={handleFeedbackSubmit}>
           <input
-            type="email"
+            type="text"
             placeholder='Enter your feedback'
             value={feedback}
             onChange={(e) => setFeedback(e.target.value)}
